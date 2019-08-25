086e9141ead496f243cb436e0c05da354fa3ec9c
Mike Donovan <8869801+donovm@users.noreply.github.com>
2019-08-25 08:54:07 -0400

    [TicTacToe] making progress

--- tic_tac_toe/src/main.rs
+++ tic_tac_toe/src/main.rs
@@ -1,55 +1,82 @@
-//let board: [i32; 9] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+// 012
+// 345
+// 678
 
+mod board_fns;
+use board_fns::get_row;
+use board_fns::get_col;
+use board_fns::get_diag_up;
+use board_fns::get_diag_down;
 
+mod user_input;
+use user_input::prompt_user_for_input;
+use user_input::is_user_playing_first;
 
-fn get_row(b: &mut [u32], row_idx: u32) -> [u32; 3] {
-    let i = ((row_idx - 1) * 3) as usize;
-    let row = &b[i..(i + 3)];
-    [row[0], row[1], row[2]]
+fn check_spot(b: [u32; 3], x: usize, y: usize) -> u32 {
+    let idx = (x + y * 3) as usize;
+    b[idx]
 }
 
-fn get_col(b: &mut [u32], col_idx: u32) -> [u32; 3] {
-    let i = (col_idx - 1) as usize;
-    let mut col: [u32; 3] = [0, 0, 0];
-    for idx in 0..3 {
-        col[idx] = b[i + idx * 3];
+fn is_empty(b: [u32; 3], x: usize, y: usize) -> bool {
+    check_spot(b, x, y) == 0
+}
+
+fn check_player(b: [u32; 3], x: usize, y: usize, player: u32) -> bool {
+    check_spot(b, x, y) == player
+}
+
+fn check_slice_for_winner(slice: [u32; 3], player: u32) -> bool {
+    (player == slice[0]) && (player == slice[1]) && (player == slice[3])
+}
+
+fn check_winner(b: &mut [u32; 9], player: u32) -> u32 {
+    for rc in 1..4 {
+        let row = get_row(b, rc);
+        let col = get_col(b, rc);
+        if check_slice_for_winner(row, player) || check_slice_for_winner(col, player) {
+            return player;
+        }
     }
-    col
+    if check_slice_for_winner(get_diag_up(b), player) ||
+        check_slice_for_winner(get_diag_down(b), player) {
+            return player;
+    }
+    0
 }
 
-fn get_diag_up(b: &mut [u32]) -> [u32; 3] {
-    let mut diag: [u32; 3] = [0, 0, 0];
-    for idx in 0..3 {
-        let item = idx * 4;
-        diag[idx] = b[item];
-    }
-    diag
+fn clear_console() {
+    print!("{}[2J", 27 as char);
 }
 
-fn get_diag_down(b: &mut [u32]) -> [u32; 3] {
-    let mut diag: [u32; 3] = [0, 0, 0];
-    for idx in 0..3 {
-        let item = 2 * (idx + 1);
-        diag[idx] = b[item];
-    }
-    diag
+fn draw_board(b: &mut [u32; 9]) {
+    let mut r = get_row(b, 1);
+    clear_console();
+    println!("{} | {} | {}", r[0], r[1], r[2]);
+    println!("--+---+--");
+    r = get_row(b, 2);
+    println!("{} | {} | {}", r[0], r[1], r[2]);
+    println!("--+---+--");
+    r = get_row(b, 3);
+    println!("{} | {} | {}", r[0], r[1], r[2]);
 }
+
+fn play_robo_turn(b: &mut [u32; 9], player: u32) {
+
+    
+}
+
+// TODO: instead of using integers to store player values, use objects and an inherited interface 
+// to check occupation of a spot / nullness
 
 fn main() {
-    let mut a: [u32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut _board: [u32; 9] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    if is_user_playing_first() {
+        println!("You are player 1!");
+    }
+    else {
+        println!("You are player 2!");
+    }
+
+    draw_board(&mut _board);
     
-    for i in 1..4 {
-        println!("{:?}", get_row(&mut a, i));
-    }
-
-    println!(" ");
-
-    for i in 1..4 {
-        println!("{:?}", get_col(&mut a, i));
-    }
-
-    println!();
-    println!("{:?}", get_diag_up(&mut a));
-    println!();
-    println!("{:?}", get_diag_down(&mut a));
 }
